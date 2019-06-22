715e7009a99975b99c71fcd867199b74205b8c56
naoya.akino <targetco25zr75@gmail.com>
2019-06-22 10:09:56 +0900

    Add validation

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
+import models.validators.TaskValidator;
 import utils.DBUtil;
 
 /**
@@ -48,6 +50,17 @@ public class UpdateServlet extends HttpServlet {
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
             t.setUpdated_at(currentTime);
 
+            //バリデーションを実行してエラーがあったら編集画面のフォームに戻る
+            List<String> errors = TaskValidator.validate(t);
+            if(errors.size() > 0){
+                em.close();
+
+                //フォームに初期値を設定、さらにエラーメッセージを送る
+                request.setAttribute("_token",  request.getSession().getId());
+                request.setAttribute("task",t);
+                request.setAttribute("errors", errors);
+            }else{
+
             //データベースを更新
             em.getTransaction().begin();
             em.getTransaction().commit();
@@ -59,7 +72,7 @@ public class UpdateServlet extends HttpServlet {
 
             //indexページへリダイレクト
             response.sendRedirect(request.getContextPath() + "/index");
-
+            }
         }
     }
 }
